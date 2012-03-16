02cc81212cce6ebf478639e165f7b2d8258c89f9
David Weikersdorfer <davidw@danvil.de>
2012-03-16 19:03:48 +0100

    Added exceptions Renamed functions

--- src/Slimage/IO.hpp
+++ src/Slimage/IO.hpp
@@ -8,15 +8,35 @@
 #ifndef SLIMAGE_IO_HPP_
 #define SLIMAGE_IO_HPP_
 
+#include <string>
+#include <stdexcept>
+
+namespace slimage
+{
+
+	struct IoException
+	: public std::runtime_error
+	{
+	public:
+		IoException(const std::string& filename, const std::string& msg)
+		: std::runtime_error("Error with file '" + filename + "': " + msg) {}
+	};
+
+}
+
 #if defined SLIMAGE_IO_QT
 	#include "detail/Qt.hpp"
 #elif defined SLIMAGE_IO_OPENCV
 	#include "detail/OpenCv.hpp"
 #endif
 
-#include <fstream>
+#if defined SLIMAGE_IO_GZ
+#include <Danvil/IO/Z.h>
+#endif
+
 #include <boost/lexical_cast.hpp>
 #include <boost/algorithm/string.hpp>
+#include <fstream>
 
 namespace slimage
 {
@@ -43,42 +63,102 @@ namespace slimage
 
 #endif
 
-	Image1ui16 Load16BitPGM(const std::string& filename) {
+	/** Loads a 16 bit 1-channel image from an ASCII PGM file */
+	Image1ui16 Load1ui16(const std::string& filename) {
+		if(!boost::algorithm::ends_with(filename, ".pgm")) {
+			throw IoException(filename, "Load1ui16 can only handle PGM files");
+		}
 		std::ifstream ifs(filename);
+		if(!ifs.is_open()) {
+			throw IoException(filename, "Could not open file");
+		}
 		std::string line;
 		std::vector<std::string> tokens;
 		// read magic line
 		getline(ifs, line);
 		boost::split(tokens, line, boost::is_any_of(" "));
-		assert(tokens.size() == 1);
-		assert(tokens[0] == "P2");
+		if(tokens.size() != 1 || tokens[0] != "P2") {
+			throw IoException(filename, "Wrong PGM file header (P2 id)");
+		}
 		// read dimensions line
 		getline(ifs, line);
 		boost::split(tokens, line, boost::is_any_of(" "));
-		assert(tokens.size() == 2);
-		unsigned int w = boost::lexical_cast<unsigned int>(tokens[0]);
-		unsigned int h = boost::lexical_cast<unsigned int>(tokens[1]);
+		unsigned int w, h;
+		if(tokens.size() != 2) {
+			throw IoException(filename, "Wrong PGM file header (width/height)");
+		}
+		try {
+			w = boost::lexical_cast<unsigned int>(tokens[0]);
+			h = boost::lexical_cast<unsigned int>(tokens[1]);
+		} catch(...) {
+			throw IoException(filename, "Wrong PGM file header (width/height)");
+		}
 		// read max line
 		getline(ifs, line);
 		boost::split(tokens, line, boost::is_any_of(" "));
-		assert(tokens.size() == 1);
+		if(tokens.size() != 1 || tokens[0] != "65535") {
+			throw IoException(filename, "Wrong PGM file header (max value)");
+		}
 		// read data
 		Image1ui16 img(w, h);
 		unsigned int y = 0;
 		while(getline(ifs, line)) {
 			boost::split(tokens, line, boost::is_any_of(" "));
-			if(tokens[tokens.size() - 1].empty()) {
+			if(tokens.back().empty()) {
 				tokens.pop_back();
 			}
-			assert(tokens.size() == w);
+			if(tokens.size() != w) {
+				throw IoException(filename, "Width and number of tokens in line do not match");
+			}
 			for(unsigned int x=0; x<w; x++) {
 				img(x,y) = boost::lexical_cast<unsigned int>(tokens[x]);
 			}
 			y++;
 		}
-		assert(y == h);
+		if(y != h) {
+			throw IoException(filename, "Height and number of lines do not match");
+		}
 		return img;
 	}
+
+	/** Saves a 1 channel 16 bit unsigned integer image to an ASCII PGM file */
+	void Save(const Image1ui16& img, const std::string& filename) {
+		if(!boost::algorithm::ends_with(filename, ".pgm")) {
+			throw IoException(filename, "Save for 1ui16 images can only handle PGM files");
+		}
+		std::ofstream ofs(filename);
+		ofs << "P2" << std::endl;
+		ofs << img.width() << " " << img.height() << std::endl;
+		ofs << "65535" << std::endl;
+		for(unsigned int y=0; y<img.height(); y++) {
+			for(unsigned int x=0; x<img.width(); x++) {
+				ofs << img(x,y);
+				if(x+1 < img.width()) {
+					ofs << " ";
+				}
+			}
+			if(y+1 < img.height()) {
+				ofs << std::endl;
+			}
+		}
+	}
+
+#if defined SLIMAGE_IO_GZ
+	Image1ui16 Load16BitGZ(const std::string& filename) {
+		// custom gzip loading!
+		PTR(Danvil::ZIO::Handle) h = Danvil::ZIO::FactorReadHandle(filename);
+		size_t width, height, channels, type;
+		h->read(width);
+		h->read(height);
+		h->read(channels);
+		h->read(type);
+		assert(channels == 1);
+		assert(type == 3);
+		Image1ui16 img(width, height);
+		h->read_raw(static_cast<void*>(img.begin()), width*height*2);
+		return img;
+	}
+#endif
 
 	void Save(const Image1ub& img, const std::string& filename) {
 		Save(Ptr(img), filename);
