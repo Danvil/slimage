76d8c7b1e2076f9f3e21f9a78c8bb38bd31ab4c1
David Weikersdorfer <davidw@danvil.de>
2012-03-29 00:10:03 +0200

    Added possibility to display image

---
+++ src/Slimage/Gui.hpp
@@ -0,0 +1,39 @@
+/*
+ * Gui.hpp
+ *
+ *  Created on: Mar 29, 2012
+ *      Author: david
+ */
+
+#ifndef SLIMAGE_GUI_HPP_
+#define SLIMAGE_GUI_HPP_
+
+#include "Slimage.hpp"
+#include "IO.hpp"
+#include "Types.hpp"
+#include <string>
+
+namespace slimage
+{
+namespace gui
+{
+
+	void Show(const std::string& caption, const Image1ub& img) {
+		cv::Mat1b img_cv(img.height(), img.width());
+		for(unsigned int y=0; y<img.height(); y++) {
+			for(unsigned int x=0; x<img.width(); x++) {
+				img_cv[y][x] = img(x,y);
+			}
+		}
+		cv::imshow(caption.c_str(), img_cv);
+		cv::waitKey(500);
+	}
+
+	void Show(const std::string& caption, const Image1f& img, float scl) {
+		Show(caption, slimage::Convert_f_2_ub(img,scl));
+	}
+
+}
+}
+
+#endif
