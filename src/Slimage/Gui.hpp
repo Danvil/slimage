aa8dd30787a33d66b251c197bab647f8eb4ce027
David Weikersdorfer <davidw@danvil.de>
2012-05-28 01:53:30 +0200

    Added overloads of Show for additional image types

--- src/Slimage/Gui.hpp
+++ src/Slimage/Gui.hpp
@@ -34,11 +34,39 @@ namespace gui
 		}
 	}
 
+	inline void Show(const std::string& caption, const Image4ub& img, unsigned int delay=200) {
+		cv::Mat3b img_cv(img.height(), img.width());
+		for(unsigned int y=0; y<img.height(); y++) {
+			for(unsigned int x=0; x<img.width(); x++) {
+				img_cv[y][x][0] = img(x,y)[2];
+				img_cv[y][x][1] = img(x,y)[1];
+				img_cv[y][x][2] = img(x,y)[0];
+			}
+		}
+		cv::imshow(caption.c_str(), img_cv);
+		if(delay > 0) {
+			cv::waitKey(delay);
+		}
+	}
+
 	inline void Show(const std::string& caption, const Image1ub& img, unsigned int delay=200) {
 		cv::Mat1b img_cv(img.height(), img.width());
 		for(unsigned int y=0; y<img.height(); y++) {
 			for(unsigned int x=0; x<img.width(); x++) {
 				img_cv[y][x] = img(x,y);
+			}
+		}
+		cv::imshow(caption.c_str(), img_cv);
+		if(delay > 0) {
+			cv::waitKey(delay);
+		}
+	}
+
+	inline void Show(const std::string& caption, const Image1ui16& img, unsigned int min, unsigned int max, unsigned int delay=200) {
+		cv::Mat1b img_cv(img.height(), img.width());
+		for(unsigned int y=0; y<img.height(); y++) {
+			for(unsigned int x=0; x<img.width(); x++) {
+				img_cv[y][x] = (255 * (static_cast<unsigned int>(img(x,y)) - min)) / (max - min);
 			}
 		}
 		cv::imshow(caption.c_str(), img_cv);
